df01cc5b0d8899741f7da977469c846ab8c528d4
Kentaro Minami <mkentaro555@gmail.com>
2020-07-21 18:31:53 +0900

    STEP11選択用フォームの作成

--- main.js
+++ main.js
@@ -24,7 +24,13 @@ const app = new Vue({
     el: '#app',
     data: {
         //どんなデータを使用するか初期値で宣言
-        todos: []
+        todos: [],
+        options: [
+            { value: -1, label: 'すべて' },
+            { value: 0, label: '作業中' },
+            { value: 1, label: '完了' },
+        ],
+        current: -1
     },
     created() {
         this.todos = todoStorage.fetch();
