11c214f736682ffaf0c34aaa64138877b8245536
Kentaro Minami <mkentaro555@gmail.com>
2020-07-21 17:06:56 +0900

    STEP1インスタンスの作成

---
+++ main.js
@@ -0,0 +1,15 @@
+const app = new Vue({
+    el: '#app',
+    data: {
+        // 使用するデータ
+    },
+    methods: {
+        // 使用するメソッド
+    },
+    filters: {
+        // 使用するメソッド
+    },
+    computed: {
+        // 使用するメソッド
+    },
+})
