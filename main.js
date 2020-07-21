11fc9285fe9dc99ed64ce82385c224689a5422f7
Kentaro Minami <mkentaro555@gmail.com>
2020-07-21 17:53:10 +0900

    STEP7リストへの追加

--- main.js
+++ main.js
@@ -3,9 +3,11 @@ var STORAGE_KEY = 'todos-vuejs-demo'
 //Storage APIを使用。
 var todoStorage = {
     fetch: function () {
+
         var todos = JSON.parse(
             localStorage.getItem(STORAGE_KEY) || '[]'
         )
+
         todos.forEach(function (todo, index) {
             todo.id = index
         })
@@ -26,6 +28,27 @@ const app = new Vue({
     },
     methods: {
         // 使用するメソッド
+        doAdd: function (event, value) {
+            // refで名前をつけておいた要素を参照
+            var comment = this.$refs.comment
+            //入力がなければ何もしないで return
+            if (!comment.value.trim().length) {
+                //フォーム要素を空にする。
+                comment.value = ''
+                return
+            }
+
+            //{新しいID, コメント, 作業状態}
+            //というオブジェクトを現在のtodosリストへpush
+            //作業状態「state」はデフォルト「作業中＝0」で作成
+            this.todos.push({
+                id: todoStorage.uid++,
+                comment: comment.value,
+                state: 0
+            })
+            //フォーム要素を空にする。
+            comment.value = ''
+        }
     },
     filters: {
         // 使用するメソッド
